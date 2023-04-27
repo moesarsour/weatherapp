6c3d3fb4d4a307263b37d6d62bbcba83648722c6
Darian Brown <browndarian011@gmail.com>
2023-04-26 21:30:43 -0400

    got open weather api to work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState, useEffect} from 'react'
+// import Comments from './components/comments'
 
 function App() {
 const [city, setCity] = useState('North Carolina');
@@ -22,23 +23,35 @@ const handleSubmit = (e) => {
 
 
 return (
-<h1 className="app py-4 text-5xl text-white font-serif">Search Weather</h1>,
- <div className="form">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter city name"
-            className="px-4 py-3"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          />
-          <button type="submit" className="px-4 py-3 bg-blue-900 text-blue-400">
-            Search
-          </button>
-        </form>
+<div className='app flex flex col items-center'>
+  <h1 className="py-4 text-5xl text-white font-serif">Search Weather</h1>,
+  <div className="form">
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        className="px-4 py-3"
+        value={city}
+        onChange={(e) => setCity(e.target.value)} />
+      <button type="submit" className="px-4 py-3 bg-blue-900 text-blue-400">
+        Search
+      </button>
+    </form>
   </div>
-
-  );
+  {weather && (
+        <div className="card bg-blue-500 text-white w-[220px] h-[350px] flex flex-col justify-center items-center mt-10">
+          <h4 className="text-2xl">{weather.name}</h4>
+          <img
+            src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+            alt=""
+            className="w-[150px]"
+          />
+          <h2 className="text-5xl font-bold mb-2">{weather.main.temp}&deg;</h2>
+          <p>{weather.weather[0].main}</p>
+        </div>
+      )}
+</div>
+);
 }
 
 export default App;
