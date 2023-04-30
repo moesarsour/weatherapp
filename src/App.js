4c6e4ff4aace199ef4576b38b84d620777b0f64a
Darian Brown <browndarian011@gmail.com>
2023-04-30 01:44:22 -0400

    edit to nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 
 import Footer from './components/footer'
 import Weather from './components/weather'
+import Nav from './components/nav'
+
 
 
 function App() {
 return (
   <div className='app'>
+    <Nav/>
   <><Weather />
   <Footer /></>
 
